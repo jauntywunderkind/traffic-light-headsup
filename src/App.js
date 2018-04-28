dbf1ae7c2494865df2a2c0d81ec3837c8ffe2e45
jauntywunderkind <jaunty+wunder+kind+git@eldergods.com>
2018-04-27 20:34:56 -0400

    Self-allocate a "user"

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from "react"
 import logo from "./logo.svg"
+import uuid from "uuid/v4"
 import "./App.css"
 
 class App extends Component {
+	componentDidMount() {
+		// assert this "user" in the history state
+		const user = this.props.user
+		if (!history.state || history.state.user !== user) {
+			const search = new URLSearchParams(location.search)
+			search.append("user", user)
+			history.replaceState(
+				{ user },
+				"Traffic Light Headsup",
+				location.pathname + "&" + search.toString()
+			)
+			// now if we go back in history we'll have this "user" in history state
+		}
+	}
 	render() {
 		return (
 			<div className="App">
@@ -15,6 +30,15 @@ class App extends Component {
 				</p>
 			</div>
 		)
+	}
+}
+
+App.defaultProps = function() {
+	const urlUser = new URLSearchParams(location.search).get("user")
+	// load "user" from location, history, or make a "new" one
+	const user = urlUser || (history.state && history.state.user) || uuid()
+	return {
+		user
 	}
 }
 

179ec33256ed57725e7147bee03a25ef6e512c4f
jauntywunderkind <jaunty+wunder+kind+git@eldergods.com>
2018-04-28 13:05:50 -0400

    Add tls server support.

--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
 import express from "express"
+import fs from "fs"
 import pos from "./pos/index"
 import movement from "./movement/sender"
 import poller from "./spat/poller"
 
-const app = express()
+const options = {}
+
+if (fs.exists("./key.pem")) {
+	options.key = fs.readFileSync("./key.pem")
+	options.cert = fs.readFileSync("./cert.pem")
+}
+
+const app = express(options)
 const port = process.env.PORT || 3001
 
 // start asking spat for data
